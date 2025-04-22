3909c3a2e60d1f1cf379cac2d825690788393ded
danielbright22 <renderhost3@gmail.com>
2025-04-22 23:23:19 +0300

    server.js

--- webrtc-chat-app/server.js
+++ webrtc-chat-app/server.js
@@ -1,63 +1,85 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
 
-// Create an Express app and HTTP server
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
-// Serve the static files (HTML, JS) in the public directory
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
 
-// Array to keep track of connected users
-let users = [];
+app.use(express.static('public'));  // serve your HTML from "public" folder
+
+let waitingUser = null;
 
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
-  users.push(socket.id);
 
-  // Broadcast offer to the next user
-  socket.on('offer', (offer) => {
-    const recipient = users.find(user => user !== socket.id);
-    if (recipient) {
-      io.to(recipient).emit('offer', offer);
+  if (waitingUser) {
+    // Pair users
+    const partner = waitingUser;
+    waitingUser = null;
+
+    socket.partner = partner;
+    partner.partner = socket;
+
+    socket.emit('matched', partner.id);
+    partner.emit('matched', socket.id);
+  } else {
+    waitingUser = socket;
+  }
+
+  socket.on('offer', (data) => {
+    if (socket.partner) {
+      socket.partner.emit('offer', data);
     }
   });
 
-  // Send the answer back to the initiator
-  socket.on('answer', (answer) => {
-    const initiator = users.find(user => user !== socket.id);
-    if (initiator) {
-      io.to(initiator).emit('answer', answer);
+  socket.on('answer', (data) => {
+    if (socket.partner) {
+      socket.partner.emit('answer', data);
     }
   });
 
-  // Handle ICE candidates
-  socket.on('candidate', (candidate) => {
-    const recipient = users.find(user => user !== socket.id);
-    if (recipient) {
-      io.to(recipient).emit('candidate', candidate);
+  socket.on('candidate', (data) => {
+    if (socket.partner) {
+      socket.partner.emit('candidate', data);
     }
   });
 
-  // Handle next button for new connection
   socket.on('next', () => {
-    console.log('User clicked Next. Finding a new partner...');
-    const newUser = users.find(user => user !== socket.id);
-    if (newUser) {
-      io.to(newUser).emit('next');
+    if (socket.partner) {
+      socket.partner.emit('disconnected');
+      socket.partner.partner = null;
+    }
+    socket.partner = null;
+
+    if (waitingUser === null) {
+      waitingUser = socket;
+    } else {
+      const partner = waitingUser;
+      waitingUser = null;
+
+      socket.partner = partner;
+      partner.partner = socket;
+
+      socket.emit('matched', partner.id);
+      partner.emit('matched', socket.id);
     }
   });
 
-  // Remove the user from the list when they disconnect
   socket.on('disconnect', () => {
+    if (socket.partner) {
+      socket.partner.emit('disconnected');
+      socket.partner.partner = null;
+    }
+    if (waitingUser === socket) {
+      waitingUser = null;
+    }
     console.log('User disconnected:', socket.id);
-    users = users.filter(user => user !== socket.id);
   });
 });
 
-// Start the server on your local IP address
-server.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on port 3000');
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
